838ac2f9c453ce80c0d2b785e184f36647c90f49
NurulAmeliaAminSI-C <146717680+NurulAmeliaAminSI-C@users.noreply.github.com>
2023-12-21 15:45:39 +0800

    upload file praktikum c9

---
+++ 231031078-Nurul Amelia Amin-tugas-9-c.py
@@ -0,0 +1,16 @@
+Biodata = {'nama'     : 'Nurul Amelia Amin',
+           'nim'      : '231031078',
+           'angkatan' : '2023',
+           'prodi'    : 'Sistem Informasi C',
+           'asal'     : 'parepare',
+           'ttl'      : 'parepare, 30-agustus-2005',
+           'alamat'   : 'jalan bau massepe No.164',
+           'hobi'     : {'hb1'   : 'membaca',
+                         'hb2'   : 'bermain game'}
+                         
+                         }
+print(Biodata)
+print(Biodata['nama']) 
+print(Biodata['nim'])
+print(Biodata['angkatan'])
+print(Biodata['hobi']['hb1'])
